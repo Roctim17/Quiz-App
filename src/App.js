8fa16a53cd14ef5f117ac46993ab666f24f68361
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-07-05 02:09:49 +0600

    fetch quiz questions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { useState } from 'react';
+import axios from 'axios';
+import Home from './Pages/Home';
+import Quiz from './Pages/Quiz';
+import Result from './Pages/Result';
 
 function App() {
-  return (
-    <div className="App">
+  const [questions, setQuestions] = useState();
+  const [name, setName] = useState();
+  const [score, setScore] = useState(0);
 
+  const fetchQuestions = async () => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=15&category=10&difficulty=easy&type=multiple`
+    );
+    setQuestions(data.results);
+  };
+  return (
+    <div className="app" >
+
+      <Routes>
+        <Route path="/" element={
+          <Home
+            name={name}
+            setName={setName}
+            fetchQuestions={fetchQuestions}
+          />}
+
+        ></Route>
+        <Route path="/quiz" element={<Quiz
+          name={name}
+          questions={questions}
+          score={score}
+          setScore={setScore}
+          setQuestions={setQuestions}
+        />}>
+
+        </Route>
+        <Route path="/result" element={
+          <Result name={name} score={score} />
+        }>
+
+        </Route>
+      </Routes>
     </div>
   );
 }
