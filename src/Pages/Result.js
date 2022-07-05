aff3e4a62cffb4d57ac45db992f4c4a2498ed32e
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-07-05 23:09:59 +0600

    result

--- src/Pages/Result.js
+++ src/Pages/Result.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import { Button } from "@material-ui/core";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-const Result = () => {
+import "./Result.css";
+
+const Result = ({ name, score }) => {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (!name) {
+            location("/");
+        }
+    }, [name, location]);
+
     return (
-        <div>
-
+        <div className="result">
+            <span className="title">Final Score : {score}</span>
+            <Button
+                variant="contained"
+                color="secondary"
+                size="large"
+                style={{ alignSelf: "center", marginTop: 20 }}
+                href="/"
+            >
+                Go to homepage
+            </Button>
         </div>
     );
 };
